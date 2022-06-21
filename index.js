97b17421668a8251545e42aeb865f5f7146702ec
sushilkattel <info@sushilkattel.com>
2022-06-21 13:52:33 -0500

    Got rid of node_modules

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cache = require("memory-cache");
 const PORT = process.env.PORT || 3080;
 
 const app = express();
-const apiKey = '';
+const apiKey = 'RGAPI-dda7b5d2-2104-48c0-b1e1-55755912e18c';
 
 app.get("/api", function (req, res) {
     const user = req.query.user
@@ -52,8 +52,26 @@ app.get("/summonerRankedQuery", async function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
     const id = req.query.user;
     const url = `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}`;
+    let resp = await customFetch(url)
+    res.send(resp)
     
 });
+//Game Query
+app.get("/summonerGameQuery", async function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    const id = req.query.user;
+    const url = `https://americas.api.riotgames.com/lol/match/v5/matches/${id}`;
+    let resp = await customFetch(url)
+    res.send(resp)
+})
+//Matchlist Query
+app.get("/summonerMatchlistQuery", async function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    const id = req.query.user;
+    const url = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${id}/ids?start=0&count=10`;
+    let resp = await customFetch(url)
+    res.send(resp)
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
